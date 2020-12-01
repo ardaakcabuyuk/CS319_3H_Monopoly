cad25c16b3c81cdaac833b614146a55b70c6614d
Ece Ünal <ece.unal@ug.bilkent.edu.tr>
2020-12-01 22:07:41 +0300

    added back buttons

--- Bilpoly/src/sample/CreditsController.java
+++ Bilpoly/src/sample/CreditsController.java
@@ -1,14 +1,17 @@
 package sample;
-
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
-public class HowToPlayController {
+public class CreditsController {
     Button backButton = new Button();
 
     @FXML
@@ -18,4 +21,5 @@ public class HowToPlayController {
         Stage window = (Stage)( ((Node) event.getSource()).getScene().getWindow());
         window.getScene().setRoot(root);
     }
+
 }
--- Bilpoly/src/sample/OptionsController.java
+++ Bilpoly/src/sample/OptionsController.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class OptionsController {
+    Button backButton = new Button();
+
+    @FXML
+    public void backButtonClicked(ActionEvent event) throws Exception {
+        System.out.println("back Button clicked. ");
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Stage window = (Stage)( ((Node) event.getSource()).getScene().getWindow());
+        window.getScene().setRoot(root);
+    }
 }
