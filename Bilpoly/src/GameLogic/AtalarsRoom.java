358c0ad1d962035a1f415efd3d6fc804394f5712
ardaakcabuyuk <ardaakcabuyuk@gmail.com>
2020-12-01 23:37:22 +0300

    minor

--- Bilpoly/src/GameLogic/AtalarsRoom.java
+++ Bilpoly/src/GameLogic/AtalarsRoom.java
@@ -4,11 +4,11 @@ import java.util.Arrays;
 
 public class AtalarsRoom extends Landable {
     //constants
-    public static final int FEE_NORMAL = -1; //TODO to be determined
-    public static final int FEE_CS = -1; //TODO to be determined
+    public final int FEE; //TODO to be determined
 
     //constructor
-    public AtalarsRoom(int index, Location location, Location[] location_available) {
+    public AtalarsRoom(int fee, int index, Location location, Location[] location_available) {
+        FEE = fee;
         type = LandableType.ATALARS_ROOM;
         this.index = index;
         this.location = new Location(location.getX(), location.getY());
@@ -22,13 +22,9 @@ public class AtalarsRoom extends Landable {
 
     //TODO change fee according to game mode;
     public void payAtalarsRoomFee(Player player) {
-//        if (GameManager.GAME_MODE == NORMAL) {
-//            player.changeMoney(-FEE_NORMAL);
-//        }
-//        else if (GameManager.GAME_MODE == CS) {
-//            player.changeMoney(-FEE_CS)
-//        }
-//        player.setInAtalarsRoom(false);
+        if (player.isInAtalarsRoom()) {
+            player.changeMoney(-FEE);
+            player.setInAtalarsRoom(false);
+        }
     }
-
 }
