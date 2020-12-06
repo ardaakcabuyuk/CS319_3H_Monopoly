32f4fd3825758e4d066101834eeb7a4dc5dc71c4
iremtekin <iremtekin67@gmail.com>
2020-12-06 13:04:52 +0300

    Revert "changed options background image change buttons"
    This reverts commit 99bb22d6403043535a54b13369447c22f0e2668a.

--- Bilpoly/src/sample/OptionsController.java
+++ Bilpoly/src/sample/OptionsController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.control.*;
 
@@ -24,23 +23,6 @@ public class OptionsController {
 
     @FXML
     Label effectsLabel;
-
-    @FXML
-    AnchorPane anchorBackground;
-
-    @FXML
-    public void image1ButtonClicked(ActionEvent event) throws Exception {
-        System.out.println("default bilkent Button clicked. ");
-        anchorBackground.getStyleClass().remove("bodybg2");
-        anchorBackground.getStyleClass().add("bodybg");
-    }
-
-    @FXML
-    public void image2ButtonClicked(ActionEvent event) throws Exception {
-        System.out.println("odeon Button clicked. ");
-        anchorBackground.getStyleClass().remove("bodybg");
-        anchorBackground.getStyleClass().add("bodybg2");
-    }
 
     @FXML
     public void backButtonClicked(ActionEvent event) throws Exception {
