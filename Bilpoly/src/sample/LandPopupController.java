a5dd9f3db8b8aa3df84a52e61aad602a725e2a9c
iremtekin <iremtekin67@gmail.com>
2020-12-18 16:17:42 +0300

    land popup classes added
    don't work for now

---
+++ Bilpoly/src/sample/LandPopupController.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class LandPopupController {
+
+    @FXML
+    public Label landName;
+
+    @FXML
+    public Label rent;
+
+    @FXML
+    public Label rentWithProperty;
+
+    @FXML
+    public Label buildProperty;
+
+    @FXML
+    public Button buyButton;
+
+}
